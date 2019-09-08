106e6f395587fb3ab4c06e08f91936a846b19f7d
omparay@gmail.com <MyU$3r1d>
2019-09-08 13:11:44 -0700

    File management and removing code that won't run on iOS 12

---
+++ iOSApp/iOSApp/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  iOSApp
+//
+//  Created by Oliver Paray on 9/8/19.
+//  Copyright © 2019 Oliver Paray. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
